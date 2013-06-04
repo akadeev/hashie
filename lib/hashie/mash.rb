494e9872b8b57e847107e14d4ef42c962aec1d6c
akadeev <a.kadeev@gmail.com>
2013-06-04 12:31:20 +0000

    Mash working

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -1,45 +1,33 @@
 module Hashie
 
   class Mash < Hash
-
-    def should_define_setter(meth, args)
-      meth.to_s.end_with?('=') && args && args.any?
-    end
-
-    def is_nested(meth)
-      meth.to_s.end_with?('!')
-    end
-
+    
     def method_missing(meth, *args, &block)
-      define_singleton_method(meth) do |*args|
-        if should_define_setter(meth, args)
-          # name=
-          # 1. remove =
-          # 2. save to hash
-          self[meth.to_s.chop] = args.first
-          p 'setter'
-          p self.inspect
-        else
-          # this is a getter
-          if is_nested(meth)
-            # obj.a!.b
-            # 1. do not remove anything from method name
-            # 2. save to hash
-            self[meth] = self.class.new
-
+      chopped = meth.to_s.end_with?('=') || meth.to_s.end_with?('!') || meth.to_s.end_with?('?') ? meth.to_s.chop : meth.to_s 
+      
+      if meth.to_s.end_with?('!')
+        define_singleton_method((chopped+'!').to_sym) do |*args|
+          if !self[chopped]
+            self[chopped] = self.class.new
           end
-          p 'getter'
-          p self.inspect
-
-          self[meth]
+          self[chopped]
+        end
+      else 
+        define_singleton_method((chopped+'=').to_sym) do |*args|
+          self[chopped] = args[0]
+        end
+        
+        define_singleton_method(chopped.to_sym) do |*args|
+          self[chopped]
+        end
+        
+        define_singleton_method((chopped+'?').to_sym) do |*args|
+          !!self[chopped]
         end
       end
 
       self.send(meth, *args)
     end
 
-    # def respond_to_missing?(method_name, include_private = false)
-    #   self.key?(method_name)
-    # end
   end
 end
--- test/lib/mash_test.rb
+++ test/lib/mash_test.rb
@@ -5,26 +5,28 @@ class MashTest < TestCase
     @mash = Hashie::Mash.new
   end
 
-  # def test_nil_when_not_set
-  #   assert_nil @mash.name
-  # end
+  def test_nil_when_not_set
+    assert_nil @mash.name
+  end
 
-  # def test_answer_is_false_when_not_set
-  #   assert_equal false, @mash.name?
-  # end
+  def test_answer_is_false_when_not_set
+    assert_equal false, @mash.name?
+  end
 
-  # def test_answer_is_true_when_set
-  #   @mash.name = "mash"
-  #   assert_equal true, @mash.name?
-  # end
+  def test_answer_is_true_when_set
+    @mash.name = "mash"
+    assert_equal true, @mash.name?
+  end
 
   def test_getset_onelevel
     @mash.name = "mash"
     assert_equal "mash", @mash.name
   end
 
-  # def test_can_define_multilevel
-  #   @mash.dog!.name = "bobik"
-  #   assert_equal "bobik", @mash.dog!.name
-  # end
+  def test_can_define_multilevel
+    assert_equal nil, @mash.dog!.name
+    @mash.dog!.name = "bobik"
+    @mash.dog!.name = "sharik"
+    assert_equal "sharik", @mash.dog!.name
+  end
 end
