055af9a986dfe912c4dc50ca06da22d5f5f507a1
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2013-06-04 22:24:15 +0400

    Mash refactoring

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -1,29 +1,18 @@
 module Hashie
 
   class Mash < Hash
-    
     def method_missing(meth, *args, &block)
-      chopped = meth.to_s.end_with?('=') || meth.to_s.end_with?('!') || meth.to_s.end_with?('?') ? meth.to_s.chop : meth.to_s 
-      
-      if meth.to_s.end_with?('!')
-        define_singleton_method((chopped+'!').to_sym) do |*args|
-          if !self[chopped]
-            self[chopped] = self.class.new
-          end
-          self[chopped]
-        end
-      else 
-        define_singleton_method((chopped+'=').to_sym) do |*args|
-          self[chopped] = args[0]
-        end
-        
-        define_singleton_method(chopped.to_sym) do |*args|
-          self[chopped]
-        end
-        
-        define_singleton_method((chopped+'?').to_sym) do |*args|
-          !!self[chopped]
-        end
+      method_name = meth.to_s
+
+      key = is_modificator(method_name) ?
+        method_name.chop : method_name
+
+      if method_name.end_with?('!')
+        define_nested(key)
+      else
+        define_setter(key)
+        define_getter(key)
+        define_try(key)
       end
 
       self.send(meth, *args)
@@ -31,6 +20,39 @@ module Hashie
 
     def respond_to_missing?(method_name, include_private = false)
       true
+    end
+
+    def define_nested(key)
+      define_singleton_method(:"#{key}!") do
+        unless self[key]
+          self[key] = self.class.new
+        end
+        self[key]
+      end
+    end
+
+    def define_setter(key)
+      define_singleton_method(:"#{key}=") do |v|
+        self[key] = v
+      end
+    end
+
+    def define_getter(key)
+      define_singleton_method(key) do
+        self[key]
+      end
+    end
+
+    def define_try(key)
+      define_singleton_method(:"#{key}?") do
+        !!self[key]
+      end
+    end
+
+    def is_modificator(name)
+      name.end_with?('=') ||
+      name.end_with?('!') ||
+      name.end_with?('?')
     end
   end
 end
