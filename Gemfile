0dee7b06d11d035c93285a7bf1f9ed55b85a5500
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2013-06-04 12:49:32 +0400

    add rake task to simplify test running, add basic 4 tests for Mash

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in hashie.gemspec
 gemspec
 
+gem 'rake'
 gem 'minitest'
 gem 'coveralls', require: false
--- test/lib/mash_test.rb
+++ test/lib/mash_test.rb
@@ -1,10 +1,28 @@
-require_relative '../test_helper'
+require 'test_helper'
 
 class MashTest < TestCase
   def setup
+    mash = Hashie::Mash.new
   end
 
-  def test_oisudgf
+  def test_nil_or_false_when_not_set
+    assert_equal mash.name?, false
+    assert_nil mash.name
+  end
 
+  def test_getset_onelevel
+    mash.name = "mash"
+
+    assert_equal mash.name, "mash"
+    assert_equal mash.name?, true
+  end
+
+  def test_getset_multilevel
+    mash.dog!.name = "bobik"
+    assert_equal mash.dog, "bobik"
+  end
+
+  def test_false_multilevel_when_not_set
+    assert_equal mash.author_.name?, false
   end
 end
